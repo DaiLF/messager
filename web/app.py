d1a4bbce372f012866f6d3319d19e0d61857c745
einsfat <liukang_20@outlook.com>
2021-07-24 17:44:44 +0800

    新增一些静态文件以及模板mobile页面

--- web/app.py
+++ web/app.py
@@ -3,7 +3,12 @@
 # @Coder  : Einsfat
 # @Date   : 2021/7/14 23:08
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
 from web.common.db.db_base import init_db
+
+templates = Jinja2Templates(directory="web/templates")
 
 
 def register_router(app: FastAPI) -> None:
@@ -14,6 +19,8 @@ def register_router(app: FastAPI) -> None:
     """
     from web.work.view import router as work_router
     app.include_router(work_router)
+    from web.mobile.views import router as mobile_router
+    app.include_router(mobile_router)
     return None
 
 
@@ -23,6 +30,7 @@ def create_app() -> FastAPI:
     :return: FastAPI()
     """
     app = FastAPI()
+    app.mount("/static", StaticFiles(directory="web/static"), name="static")
     init_db()
     register_router(app)
     return app
--- web/mobile/views.py
+++ web/mobile/views.py
@@ -2,3 +2,13 @@
 # @File   : views.py.py
 # @Coder  : Einsfat
 # @Date   : 2021/7/23 17:22
+from fastapi import APIRouter
+from starlette.requests import Request
+from web.app import templates
+
+router = APIRouter()
+
+
+@router.get("/message", tags=["Mobile"])
+async def message(request: Request):
+    return templates.TemplateResponse("mobile/index.html", {"request": request})
